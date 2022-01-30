33fab2cc0339047641be52187e38e8c840698902
Matouš Bien <matous.bien.020@pslib.cz>
2022-01-30 16:58:01 +0100

    Přidejte soubory projektu.

---
+++ aritmeticka posloupnost/aritmeticka posloupnost.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	
+int diference = 0;
+int predchozi = 0;
+
+		printf("Zadejte prvni clen Aritmeticke poslopnosti: ");
+	scanf("%d", &predchozi);
+
+		printf("\nZadejte druhy clen Aritmeticke posloupnosti: ");
+	scanf("%d", &diference);
+
+		printf("\n");
+
+	for (int x = 1; x <= 5; x++) {
+		printf("Clen cislo: %d: %d\n", x, predchozi);
+		predchozi += diference;
+	}
+}
